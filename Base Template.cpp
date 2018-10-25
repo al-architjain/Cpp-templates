83d9c8d27073ed85ef121d7e71cebfdff9177edb
Archit Jain <20044184+al-architjain@users.noreply.github.com>
2018-10-25 13:45:13 +0530

    Create Base Template.cpp

---
+++ Base Template.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll  long long
+#define ull unsigned long long
+#define mod 1000000007
+using namespace std;
+
+int main(){
+    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    ll T;
+    cin>>T;
+    while(T--){
+        
+    }
+    
+    return 0;
+}
