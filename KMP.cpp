8307b3620bda7224b8355ab84a5f27ff21e62a77
Archit Jain <architjain.delhi2@gmail.com>
2019-06-05 19:04:42 +0530

    KMP template

--- KMP.cpp
+++ KMP.cpp
@@ -1,35 +1,37 @@
-vector<int> computePrefix(string pattern){
-    int m = pattern.size();
-    vector<int> longestPrefix(m);           // index where to jump back to.
-    for(int i = 1, j = 0;  i < m;  i++){
-        while(j > 0 && pattern[j] != pattern[i]){
-            j = longestPrefix[j - 1];
-        }
-        if(pattern[i] == pattern[j]){
-            j++;
-            longestPrefix[i] = j;
-        }
-        else{
-            longestPrefix[i] = j;
-        }
-    }
+vector<int> calculateLPS(string pattern){
+	int m = pattern.size();
+	vector<int> lps(m,0);
+	
+	for(int i=1, j=0; i<m; i++ ){
+		while(j>0 && pattern[j]!=pattern[i]){
+			j = lps[j-1];		
+		}
+		if(pattern[i] == pattern[j] ){
+			j++;
+			lps[i] = j;
+		}
+		else{
+			lps[i] = j;			//or lps[i]=0	as j=0;
+		}
+	}
+	return lps;
 }
 
+void KMP(string str, string pattern){
+	int n = str.size();
+	int m = pattern.size();
+	vector<int> lps = calculateLPS(pattern);
 
-void KMP(string str,string pat){
-    int n = str.size();
-    int m = pat.size();
-    vector<int> longestPrefix = computePrefix(pat);
-    for(int i = 0, k = 0; i < n; i++){
-        while(k > 0 && pat[k] != str[i]){
-            k = longestPrefix[k - 1];
-        }
-        if(str[i] == pat[k]){
-            k++;
-        }
-        if(k == m){
-            cout << i - m + 1 << "\n";
-            k = longestPrefix[k - 1];
-        }
-    }
+	for(int i=0; j=0; i<n; i++){
+		while( j>0 && pattern[j]!=str[i]){
+			j = lps[j-1];		
+		}
+		if(str[i] == pattern[j]){
+			j++;
+		}
+		if( j == m ){
+			cout<<i-m+1<<"\n";
+			j = lps[j-1];
+		}
+	}
 }
