666f61f1e5f9fde3d98ffe914e860cfe0ff40fc7
Archit Jain <architjain.delhi2@gmail.com>
2019-09-08 19:08:34 +0530

    longest Increasing Subsequence finding in an array

--- Dynamic Programming/longestIncreasingSubsequence.cpp
+++ Dynamic Programming/longestIncreasingSubsequence.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 #include<vector>
 #include<math.h>
+#include<limits.h>
 using namespace std;
 
 class longestIncreasingSubsequence{
 	int len;
 	vector<int> arr;
 	vector<int> dp;
+	vector<int> prev;
 	public:
 		longestIncreasingSubsequence(){
 			len=0;
@@ -15,15 +17,20 @@ class longestIncreasingSubsequence{
 			cin>>len;
 			arr.resize(len);
 			dp.resize(len,1);
+			prev.resize(len);
 			for(int i=0; i<len; i++){
 				cin>>arr[i];
+				prev[i] = i;
 			}
 		}
 		int bottomUp(){
 			for(int i=1; i<len; i++){
 				for(int j=i-1; j>=0; j--){
 					if(arr[j]<arr[i]){
-						dp[i] = max(dp[i], dp[j]+1);
+						if(dp[j]+1 > dp[i]){
+							dp[i] = dp[j]+1;
+							prev[i] = j;
+						}
 					}
 				}
 			}
@@ -31,6 +38,19 @@ class longestIncreasingSubsequence{
 		}
 		void output(){
 			cout<<bottomUp()<<endl;
+			int maxindex=-1;
+			int maxnum = INT_MIN;
+			for(int i=0; i<len; i++){
+				if(dp[i]>=maxnum){
+					maxnum = dp[i];
+					maxindex = i;
+				}
+			}
+			int i = maxindex;
+			while(maxnum--){
+				cout<<arr[i]<<" ";
+				i = prev[i];
+			}
 		}
 };
 
